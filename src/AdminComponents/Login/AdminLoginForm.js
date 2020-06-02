0cc7a2284759fa2ff4bea5f8eb0de8cc1c7f5537
adamandpdma <40270042+adamandpdma@users.noreply.github.com>
2020-06-02 13:48:23 +0800

    Update AdminLoginForm.js

--- src/AdminComponents/Login/AdminLoginForm.js
+++ src/AdminComponents/Login/AdminLoginForm.js
@@ -85,24 +85,16 @@ const form = withStyles(styles)(props => {
   return (
     <React.Fragment>
           <header style={{"backgroundColor": "black"}}>
-      {/* <svg xmlns="http://www.w3.org/2000/svg" width="515.456" height="72" viewBox="0 0 515.456 72"> */}
-      {/* <g id="CloudBoard_" data-name="CloudBoard " transform="translate(-651.984 -1005)">
-      </g> */}
+
       <img src = {Logo}
       width="280" height="72"/>
 
 
-    {/* </svg> */}
     </header>
     <Container style={bgStyle}>
 
 
-    {/* <Box
-        position="absolute"
-        top = {-5}
-        right="3%"    > 
-    <Link to={`./EmployeeRegisterInput`}>Register</Link> 
-    </Box>    */}
+
     <form onSubmit={handleSubmit}>
     <Container
     style={{borderRadius: '10px'}}
@@ -202,133 +194,3 @@ const form = withStyles(styles)(props => {
 
 
 export default form
-
-
-// import React, {Component} from 'react'
-// import './Form.css'
-// import Container from '@material-ui/core/Container';
-// import { TextField, Typography } from '@material-ui/core';
-// import Button from '@material-ui/core/Button';
-// import { withStyles } from '@material-ui/core/styles';
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-// import Popup from './Popup'
-
-// class Form extends Component {
-//     constructor (props) {
-//         super(props);
-//         this.state = {
-//             Id : "",
-//             Password : ""
-//         }
-//       }
-
-//       handleUserInput = (e) => {
-//         const name = e.target.name;
-//         const value = e.target.value;
-//         this.setState({[name]: value})
-//       }
-
-//       handleSubmit = (event) => {
-//           const checkId = this.state.Id;
-//           const checkPassword = this.state.Password;
-//           if (checkId.length < 6) {
-//             alert("Id is not long enough");
-//             event.preventDefault();
-//             return;
-//         }
-          
-//           if (checkPassword.length < 8) {
-//               alert("Password is not long enough")
-//               event.preventDefault();
-//               return;
-//           }
-
-//       }
-    
-
-//     render(){
-
-//       const { classes } = this.props;
-
-//       return(
-//         <Container 
-//         component="main" 
-//         maxWidth="xs"
-//         >
-//         <div>
-//             <Typography variant = "h4"
-//             className={classes.container}>
-//             Login Page
-//             </Typography>
-//         </div>
-//         <form 
-//         onSubmit={this.handleSubmit}>
-//         <div>
-//             <TextField
-//             input type = "number"
-//             variant="outlined"
-//             margin="normal"
-//             fullWidth
-//             InputProps={{
-//               classes: {
-//                 notchedOutline: classes.notchedOutline
-//               }
-//             }}
-//             className = "textfield"
-//             name="Id"
-//             placeholder = "Id" 
-//             value = {this.state.Id} 
-//             onChange={this.handleUserInput}
-//             />  
-//             {/* <label htmlFor id> Id </label>
-//             <input type = "number" 
-//             name="Id"
-//             placeholder = "Id" 
-//             value = {this.state.Id} 
-//             onChange={this.handleUserInput}></input> */}
-//           </div>
-//           <div>
-//           <TextField
-//             variant="outlined"
-//             margin="normal"
-//             fullWidth
-//             InputProps={{
-//               classes: {
-//                 notchedOutline: classes.notchedOutline
-//               }
-//             }}
-//             className = "textfield"
-//             name="Password"
-//             placeholder = "Password" 
-//             value = {this.state.Password} 
-//             onChange={this.handleUserInput}
-//             />      
-//           {/* <label htmlFor password>Password</label>
-//           <input type = "password" 
-//           name="Password"
-//           placeholder = "Password" 
-//           value = {this.state.Password} 
-//           onChange = {this.handleUserInput}></input> */}
-//           </div>
-//           <div>
-//           <Button
-//             type="submit"
-//             margin="normal"
-//             fullWidth
-//             variant="contained"
-            // className={classes.button}>
-//           <LockOutlinedIcon /> Login
-//           </Button>
-//           <Popup></Popup>
-//             {/* <input type = "submit" 
-//             value="Login"></input> */}
-//           </div>
-//         </form>
-//         </Container>
-//       )
-//     }
-// }
-
-//   export default withStyles(styles) (Form)
-
-
